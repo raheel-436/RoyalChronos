cfe21eafef9827179b28dfea4e6f276114d6f904
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-08-08 12:07:56 +0500

    'Added addToBag button to Items'

--- scripts/script.js
+++ scripts/script.js
@@ -12,16 +12,19 @@ function displayItemOnHomePage() {
   items.forEach((item) => {
     innerHtml += `
       <div class="item-container">
-          <img
-            src="${item.image}"
-            alt="Watch Image"
-            class="item-image"
-          />
+          <div class="image-container">
+            <img
+              src="${item.image}"
+              alt="Watch Image"
+              class="item-image"
+            />
+            <button class="addToBag-btn">Add to Bag</button>
+          </div>
           <div class="item-name">
-            ${item.item_name}
+              ${item.item_name}
           </div>
           <div class="item-price">Rs ${item.original_price}</div>
-        </div>
+      </div>
    `;
   });
   itemsContainerElement.innerHTML = innerHtml;
